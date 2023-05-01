60ac43ec06dd4c6191b77b1e87a080e2e42dc7cb
GhostSec322 <GhostSec322@gmail.com>
2023-05-01 15:37:04 +0900

    2023-05-01 Study_JavaScript: Object

--- app.js
+++ app.js
@@ -38,3 +38,43 @@ const amIFat=true;
 const amIman=false;
 console.log(amIFat);
 console.log(amIman);
+// Arrays 
+/**
+ * Data 나열 방법중 하나 
+ * []안에 ,로 데이터들을 나열한다. 
+ * 변수 사용가능하며 Boolean 형 또한 사용가능하다. 
+ */
+const mon="mon";
+const tue ="tue";
+const wed ="wed";
+const thu ="thu";
+const fri ="fri";
+const sat ="sat";
+const sun ="sun";
+const day_of_week=[mon,tue,wed,thu,fri,sat,sun];
+console.log(day_of_week);
+const Day_Week =["Mon","Tue","Wed","Thu","Fri","Sat"];
+console.log(Day_Week);
+console.log(Day_Week[0]);
+console.log(Day_Week[5]);
+//Insert Data in Day_Week
+Day_Week.push("Sun");
+console.log(Day_Week);
+Day_Week.pop("Sun");
+console.log(Day_Week);
+/**
+ * Object
+ * Proprty 를 가진 데이터를 저장하며 {}를 이용한다. 
+ */
+const Player ={
+    Name: " Zombi",
+    age :30,
+    Team :" ISEC",
+    wappon:"Knife"
+};
+console.log(Player.Name);
+console.log(Player["Name"]);
+console.log(Player.age);
+console.log(Player["age"]);
+console.log(Player.Team);
+console.log(Player["Team"]);
